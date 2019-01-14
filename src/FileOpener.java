0676f6bbf24a7209125eb2eceb8562b8930ed8a8
sanduvla <michael.connor@mail.utoronto.ca>
2019-01-14 18:30:53 -0500

    Created project files and started FileOpener class

---
+++ src/FileOpener.java
@@ -0,0 +1,21 @@
+/** Opens a given file and returns a string consisting of the entire files contents. Prints to the console
+ * if the file cannot be located.
+ *
+ * @author Michael Connor
+ */
+public class FileOpener
+{
+    // Holds the contents of the opened file.
+    String filecontents;
+
+    /** Constructor takes file location as input, opens the file, copies contents to filelocation global String, then
+     * closes the file.
+     *
+     * @author Michael Connor
+     * @param location - String containing the location of the file to be opened
+     */
+    public FileOpener(String location)
+    {
+
+    }
+}
